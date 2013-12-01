5648d2371cf2035d53b00a138bd13a822b5dfe51
jonasagx <jonas.agx@gmail.com>
2013-12-01 10:51:09 -0300

    New version, I can't explain now.

--- src/Handler.java
+++ src/Handler.java
@@ -1,40 +1,107 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.Stack;
 
 class Handler {
     private static ServerSocket s = null;
     private Socket client = null;
-    private ArrayList<Socket> clients = new ArrayList<Socket>();
     
     public Handler(){
     	s = createServer(8080);
     }
 
-    //It waits for a new socket connection and trow it for a thread
+    //It waits for a new socket connection and throw it for a thread
     public void listen(){
+    	Processor p = new Processor();
+    	p.start();
+    	
     	System.out.println("Listining on port " + s.getLocalPort());
+    	
     	while(true){
     		try{
     			this.client = s.accept();
-    			System.out.println("Connections: " + clients.size());
-    			clients.add(this.client);
-    			new Transeunte(this.client);
+    			p.append(this.client);
     		} catch (IOException e){
     			System.out.println(e.getMessage());
+    			p.interrupt();
     			break;
     		}
     	}
     }
 
     private ServerSocket createServer(int port){
-	ServerSocket tmp = null;
-        try{
+    	ServerSocket tmp = null;
+        
+    	try{
             tmp = new ServerSocket(port);
         } catch(IOException e){
             System.err.println(e.getMessage());
         }
         return tmp;
     }
+}
+
+class Processor extends Thread{
+    private BufferedReader request = null;
+	private DataOutputStream response = null;
+
+	private Stack<Socket> clients = null;
+	private boolean busy = false;
+	
+	public Processor(){
+		this.clients = new Stack<Socket>();
+	}
+	
+	@Override
+	public void run(){
+		System.out.println("Processor is running");
+		while(true){
+			if(this.clients.size() > 0){
+				this.process();
+			}
+			try {
+				sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void process() {
+		Socket client = this.clients.pop();
+		
+		try{
+			request = new BufferedReader(
+					new InputStreamReader(client.getInputStream()));
+			
+			response = new DataOutputStream(client.getOutputStream());
+			
+			StringBuilder r = new StringBuilder();
+			String tmp;
+						
+			while( !(tmp = request.readLine()).equals("") ){				
+				r.append(tmp);
+				System.out.println(tmp);
+			}
+			
+			response.writeBytes( Interpreter.understand(r) );
+			client.close();
+		} catch (IOException e){
+			System.err.println(e.getMessage() + "\n" + e.getStackTrace());
+		}
+	}
+
+	public void append(Socket s){
+		this.clients.add(s);
+		this.busy = true;
+	}
+	
+	public boolean state(){
+		return this.busy;
+	}	
 }
--- src/Interpreter.java
+++ src/Interpreter.java
@@ -4,20 +4,54 @@ public class Interpreter {
     private static String[] tokens = {"GET", "POST", 
         "HEAD", "PUT", "DELETE", "TRACE", "CONNECT", "PATCH"};
     
-    public static String understand(String http_request){
-        String[] raw = http_request.split(" ");
-        for(String text : raw){
-            for(String token : tokens){
-                if(text.equals(token)){
-                    
-                }
-            }
+    /*
+     * Structure of an HTTP request:
+     * 
+     * COMMAND /ADDRESS HTTP-VERSION
+     * 
+     * Example:
+     * GET / HTTP/1.1
+     */
+    
+    @SuppressWarnings("null")
+	public static String understand(StringBuilder http_request){
+        if(http_request != null || http_request.length() > 0){        	
+        	String[] raw = http_request.toString().split(" ");
+        	
+        	switch(raw[0]){
+        		case "GET": return doGet(http_request);
+        		
+        		default: return "HTTP/1.1 200 OK" +
+								"Date: Mon, 23 May 2005 22:38:34 GMT" +
+								"Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)" +
+								"Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT" +
+								"Etag: '3f80f-1b6-3e1cb03b'" +
+								"Content-Type: text/html; charset=UTF-8" +
+								"Content-Length: 131" +
+								"Connection: close" +
+								"\n\n" +
+								"<html><head>" + 
+								"<title>An Example Page</title>" +
+								"</head><body>" +
+								"Hello World, this is a very simple HTML document." +
+								"</body></html>";
+        	}
         }
-        return null;
+        return "";
     }
     
-    public static String doGet(String request){
-    	System.out.println(request);
-        return "Response\n";
+    public static String doGet(StringBuilder request){
+    	System.out.println("Done");
+    	String response = "HTTP/1.1 200 Ok" +
+				"Date: Mon, 23 May 2005 22:38:34 GMT" +
+				"Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)" +
+				"Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT" +
+				"Etag: '3f80f-1b6-3e1cb03b'" +
+				"Content-Type: text/html; charset=UTF-8" +
+				"Content-Length: " + request.toString().length() + 
+				"Connection: close" +
+				"\n\n" +
+				request.toString();
+    	return response;
     }
 }
